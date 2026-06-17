4f0c07008aba449c933988b53399775a4223374e
shiiman <hsnonsense5@gmail.com>
2026-06-17 13:15:04 +0900

    feat: Astroを導入し基本設定を追加

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://shiimanblog.com',
+  trailingSlash: 'always',
+  build: { format: 'directory' },
+  integrations: [sitemap()],
+})

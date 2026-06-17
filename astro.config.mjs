7c54de5a82624f63f46b42a77e33ce3284f57d14
shiiman <hsnonsense5@gmail.com>
2026-06-17 13:20:12 +0900

    fix: sharpをAstro想定の^0.34.0へ整合しoutput:staticを明示

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shiimanblog.com',
+  output: 'static',
   trailingSlash: 'always',
   build: { format: 'directory' },
   integrations: [sitemap()],
